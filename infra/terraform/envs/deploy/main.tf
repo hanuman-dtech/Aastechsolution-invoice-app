03f0639db0ba2590fd8e237c06befe044e029318
hanuman-dtech <sait.devopstech@gmail.com>
2026-03-01 23:05:00 -0500

    fix(deploy): enable DEBUG=true for API docs
    Co-authored-by: thatisai <sairamrao9479@gmail.com>

--- infra/terraform/envs/deploy/main.tf
+++ infra/terraform/envs/deploy/main.tf
@@ -188,7 +188,7 @@ resource "azurerm_container_app" "backend" {
       }
       env {
         name  = "DEBUG"
-        value = "false"
+        value = "true"
       }
       env {
         name  = "CORS_ORIGINS"
